729d7406dbf808d376cef1cee01b5421b7af8dbb
Wangui-quinnie <w.wangui@ymail.com>
2023-09-26 05:56:01 -0700

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,9 +23,12 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[small])
 				small = j;
 		}
-		temp = array[small];
-		array[small] = array[i];
-		array[i] = temp;
-		print_array(array, size);
+		if (small != i)
+		{
+			temp = array[i];
+			array[i] = array[small];
+			array[small] = temp;
+			print_array(array, size);
+		}
 	}
 }
